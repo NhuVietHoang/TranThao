cac25e8c9f3807291c2bf1aca118181f05d046c4
HoangNV <viethoangnhu@gmail.com>
2025-02-27 14:19:32 +0700

    test

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Cho mình bé",
-    introTitle: 'Ngày Thầy Thuốc Việt Nam',
-    introDesc: `Chúc bé hết đau bụng , ăn uống lành mạnh và mai không bị bố mắng nhé :))`,
-    btnIntro: '^^Oki không ạ^^',
+    introTitle: 'Gủi vợ nhá :))',
+    introDesc: ` Từ cái ngày mưa gặp em ở thư viện cho em mượn chiếc ô a đã vô tình đưa cả trái tim mình cho em rồi :)), nếu có thể hãy mở lòng với anh nhé`,
+    btnIntro: '^^ Dạaaa ^^',
     title: 'Thời gian quen nhau chưa lâu 🥰',
     desc: 'Nhưng mà anh nghĩ em cũng có chút cảm tình với anh đúng không (đố em trả lời được không đấy :)) 🥰 ',
     btnYes: 'Dạ đúng ạ <33',
     btnNo: 'Không nha :3',
     question: 'Đúng là anh đẹp trai thật :)) nên em nghĩ sao 1 buổi phớt đết ơ tương lai gần ạ',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Chắc chắn rồi anh, em sắp mở lòng rồiiii',
+    reply: 'Chắc chắn rồi  chồng yêu owiii, em cũng mong lắm ajaaaa :))',
     mess: 'Anh biết mà 🥰. Hạ hạ 😘😘',
-    messDesc: 'Chúc em mau khỏi ốm nhé,có gì in bốc anh ở đây ạ',
+    messDesc: 'Có gì đết đủng in bốc anh ở đây ạ',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100087509435346'
+    messLink: 'https://www.instagram.com/hoangnv_112000?igsh=eXpkcWV5NjNrbG54&utm_source=qr'
 }
