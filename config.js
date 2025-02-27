a4b80bddfef94943c750f008bdf0d054703e777d
HoangNV <viethoangnhu@gmail.com>
2025-02-27 14:06:02 +0700

    test

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Cho mình bé",
+    introTitle: 'Ngày Thầy Thuốc Việt Nam',
+    introDesc: `Chúc bé hết đau bụng , ăn uống lành mạnh và mai không bị bố mắng nhé :))`,
+    btnIntro: '^^Oki không ạ^^',
+    title: 'Thời gian quen nhau chưa lâu 🥰',
+    desc: 'Nhưng mà anh nghĩ em cũng có chút cảm tình với anh đúng không (đố em trả lời được không đấy :)) 🥰 ',
+    btnYes: 'Dạ đúng ạ <33',
+    btnNo: 'Không nha :3',
+    question: 'Đúng là anh đẹp trai thật :)) nên em nghĩ sao 1 buổi phớt đết ơ tương lai gần ạ',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Chắc chắn rồi anh, em sắp mở lòng rồiiii',
+    mess: 'Anh biết mà 🥰. Hạ hạ 😘😘',
+    messDesc: 'Chúc em mau khỏi ốm nhé,có gì in bốc anh ở đây ạ',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100087509435346'
+}
